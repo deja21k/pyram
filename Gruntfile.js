1fe6d6c26e9360af4edc59c2aab7b707e7eda36a
Bojan Lazic <lazicbojanns@gmail.com>
2015-11-18 16:51:19 +0100

    parallax

--- Gruntfile.js
+++ Gruntfile.js
@@ -253,16 +253,16 @@ module.exports = function (grunt) {
     },
 
     // The following *-min tasks produce minified files in the dist folder
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= config.app %>/images',
-          src: '{,*/}*.{gif,jpeg,jpg,png}',
-          dest: '<%= config.dist %>/images'
-        }]
-      }
-    },
+      imagemin: {
+        dist: {
+          files: [{
+            expand: true,
+            cwd: '<%= config.app %>/images',
+            src: ['**/*.{jpeg,jpg,png}'],
+            dest: '<%= config.dist %>images'
+          }]
+        }
+      },
 
     svgmin: {
       dist: {
