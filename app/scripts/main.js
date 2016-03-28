b7e0cd080313ea318e4d7d1eaad88e8f89d4a889
deja21k <thedoctor92@hotmail.com>
2016-03-28 21:20:03 +0200

    SmoothScroll in progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -47,6 +47,21 @@ $(document).ready(function() {
   });
 });
 
+/*
+  Slidemenu
+*/
+(function() {
+	var $body = document.body
+	, $menu_trigger = $body.getElementsByClassName('navbar-toggle')[0];
+
+	if ( typeof $menu_trigger !== 'undefined' ) {
+		$menu_trigger.addEventListener('click', function() {
+			$body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
+		});
+	}
+
+}).call(this);
+
 
 // /***************** Smooth Scrolling ******************/
 //
@@ -67,3 +82,28 @@ $(document).ready(function() {
 //   });
 //
 // });
+$(function(){
+
+	var $window = $(window);		//Window object
+
+	var scrollTime = 1.2;			//Scroll time
+	var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+
+	$window.on("mousewheel DOMMouseScroll", function(event){
+
+		event.preventDefault();
+
+		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+		var scrollTop = $window.scrollTop();
+		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+		TweenMax.to($window, scrollTime, {
+			scrollTo : { y: finalScroll, autoKill:true },
+				ease: Power1.easeOut,	//For more easing functions see http://api.greensock.com/js/com/greensock/easing/package-detail.html
+				autoKill: true,
+				overwrite: 5
+			});
+
+	});
+
+});
